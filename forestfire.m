3d425237bf5245747cfa36f841d905760978c8a9
Jort20 <131657231+Jort20@users.noreply.github.com>
2023-06-27 14:16:30 +0200

    Update forestfire.m
    Full working fast code

--- forestfire.m
+++ forestfire.m
@@ -1,39 +1,37 @@
 numrows = 50;    % number of rows
 numcols = 100;   % number of columns
 
-% initialize the world randomly
-world = zeros(numrows, numcols);  % 0: white, 1: green, 2: oranje 3;rood zwart
 
 
-tree = 0.01;   % probability of a cell becoming a tree
+% 2 = leeg
+% 1 = vuur
+% 0 = boom
+% 3 = bes
+#world = zeros(numrows, numcols);  % 0: white, 1: green, 2: orange, 3: red/black
+world = repmat(2, [numrows, numcols]);
+neighborhood = [0 1 0; 1 0 1; 0 1 0];
+
+
+
+tree = 0.001;   % probability of a cell becoming a tree
 fire = 0.0001;  % probability of a tree catching fire
 
-while (1),
+while (1)
+ new_world = world;
+ #stop = true;
   % apply the rules to update the world
-  new_world = world;
-  for row = 1:numrows
-    for col = 1:numcols
-      currentpixel = world(row, col);
-      neighbors = world(max(row-1,1):min(row+1,numrows), max(col-1,1):min(col+1,numcols));
-      greenneighbors = sum(neighbors(:) == 1);
-      redneighbors = sum(neighbors(:) == 2);
-      if currentpixel == 1 && redneighbors >= 1
-        new_world(row, col) = 2;  % Tree lights on fire
-      elseif currentpixel == 0
-        if rand() < tree
-          new_world(row,col) = 1;  % Becomes a tree
-        end
-      elseif currentpixel == 1
-        if rand() < fire
-          new_world(row,col) = 2;  % Lights on fire
-        end
-      elseif currentpixel == 2
-        new_world(row, col) = 0;  % Cell died
-      end
-    end
-  end
+  N_fire = conv2(new_world == 1, neighborhood, 'same');
+    
+    
+  new_world(world == 0 & N_fire >= 1) = 1;
+  new_world(world == 2 & rand(numrows, numcols) < tree) = 0;
+  new_world(world == 0 & rand(numrows, numcols) < fire) = 1;
+  new_world(world == 1) = 2;
+
+
+
   world = new_world;
-  colormap([1 1 1; 0 1 0; 1 0 0]);  % white, green, red
+  colormap([0 1 0 ;1 0 0 ; 1 1 1]);  % green, red, white
   imagesc(world, [0 2]);
   drawnow;
 end
